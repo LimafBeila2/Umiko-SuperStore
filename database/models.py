c64ce1190580a9ab72677590398d7e10de57cdd4
LimafBeila2 <golbolidary002@gmail.com>
2025-03-04 06:44:05 +0400

    sdasdddasd

--- database/models.py
+++ database/models.py
@@ -1,29 +1,27 @@
 import asyncio
 import os
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 from dotenv import load_dotenv  # Для загрузки переменных из .env файла
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
-from sqlalchemy import BigInteger, String, DateTime, Column
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-# Установите переменные окружения
+# Загружаем переменные окружения из .env файла
+load_dotenv()
+
+# Переменные окружения для подключения к PostgreSQL
 PGHOST = os.getenv('PGHOST', 'postgres.railway.internal')
 PGPORT = os.getenv('PGPORT', '5432')
 PGUSER = os.getenv('PGUSER', 'postgres')
-PGPASSWORD = os.getenv('PGPASSWORD', 'ilJVkITTuilDrVCNGqBaTzaMRMxhwOuI')
+PGPASSWORD = os.getenv('PGPASSWORD', '')
 PGDATABASE = os.getenv('PGDATABASE', 'railway')
 
 # Формирование строки подключения
 DATABASE_URL = f"postgresql+asyncpg://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATABASE}"
 
-# Создание двигателя и сессии
-engine = create_async_engine(DATABASE_URL)
-
-async_session = async_sessionmaker(engine)
-
+# Создание асинхронного движка и сессии
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 # Базовый класс для моделей
 class Base(AsyncAttrs, DeclarativeBase):
@@ -40,23 +38,15 @@ class Product(Base):
     last_checked_price = Column(Float)
     last_checked = Column(DateTime, default=datetime.utcnow)
 
-# Создаем асинхронный движок и сессию для PostgreSQL
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-async_session = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
-
-# Функция для создания таблицы, если её нет
+# Функция для создания таблицы
 async def create_table():
-    # Создаем таблицу только если она не существует
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     print("Таблица успешно создана!")
 
-# Пример асинхронной работы с базой
+# Асинхронный запуск
 async def main():
     await create_table()
 
-# Запуск асинхронного кода
 if __name__ == "__main__":
     asyncio.run(main())
