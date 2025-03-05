f8e566dcedd04916eeb85fd4205f66930c09b3d3
Your Name <your.email@example.com>
2025-03-05 08:29:00 +0400

    свываывавыа

--- main.py
+++ main.py
@@ -15,23 +15,18 @@ from database.models import async_session, Product
 from sqlalchemy.future import select
 import logging
 from selenium.common.exceptions import TimeoutException
-from sqlalchemy.exc import OperationalError
-import time
 
+# Настройки логирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Настройки Chrome
 options = Options()
-options.binary_location = "/usr/bin/chromium"
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-blink-features=AutomationControlled")
 
-# Используем системный chromedriver
-service = Service("/usr/bin/chromedriver")
-
-# Создаем веб-драйвер
+# Автоустановка ChromeDriver
+service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
+
 
 # Функция для входа в аккаунт Umico Business с использованием данных из .env
 def login_to_umico(driver):
@@ -57,9 +52,9 @@ def login_to_umico(driver):
 
     try:
         WebDriverWait(driver, 30).until(EC.url_contains("/account/orders"))
-        print("Успешный вход в Umico Business! Вы находитесь на странице заказов.")
+        logging.info("Успешный вход в Umico Business! Вы находитесь на странице заказов.")
     except Exception as e:
-        print(f"Ошибка входа: {e}")
+        logging.error(f"Ошибка входа: {e}")
         driver.quit()
         raise ValueError("Ошибка входа! Проверь логин и пароль.")
 
@@ -68,77 +63,48 @@ def login_to_umico(driver):
 async def get_product_urls():
     async with async_session() as session:
         async with session.begin():
-            # Выполняем запрос для получения всех продуктов
             result = await session.execute(select(Product.id, Product.product_url, Product.edit_url))
-            products = result.fetchall()  # Получаем все строки
-            return products  # Возвращаем список кортежей с данными (id, product_url, edit_url)
+            products = result.fetchall()
+            return products
 
 
-# Функция для закрытия рекламы на странице
+# Функция для закрытия рекламы на странице (поиск города на двух языках)
 async def close_ad(driver):
     try:
         baku_option = WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable((By.XPATH, "//span[text()='Баку' or text()='Bakı']"))
         )
         baku_option.click()
-        print("Город Баку выбран.")
+        logging.info("Город Баку выбран.")
     except Exception:
-        print("Окно выбора города не появилось, продолжаем выполнение кода.")
+        logging.info("Окно выбора города не появилось, продолжаем выполнение кода.")
 
 
+# Функция для клика по элементу с двумя вариантами текста
 def click_element_by_text(text1, text2):
-    # Используем XPath с условием "или" для поиска обоих текстов
-    element = driver.find_element(By.XPATH, f"//a[contains(text(), '{text1}') or contains(text(), '{text2}')]")
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, f"//a[contains(text(), '{text1}') or contains(text(), '{text2}')]")
+        )
+    )
     actions = ActionChains(driver)
     actions.move_to_element(element).perform()
     element.click()
 
 
-# Функция для проверки существования таблицы в базе данных
-async def wait_for_table_creation():
-    max_retries = 10
-    retries = 0
-    while retries < max_retries:
-        try:
-            async with async_session() as session:
-                async with session.begin():
-                    # Проверяем наличие таблицы, выполняя запрос
-                    result = await session.execute(select(Product.id).limit(1))  # Просто пробуем запросить данные
-                    result.fetchall()  # Проверка, есть ли записи
-                    print("Таблица существует!")
-                    return True
-        except OperationalError:
-            # Если таблица не существует, ждем некоторое время и пробуем снова
-            print("Таблица не существует, ждем...")
-            retries += 1
-            await asyncio.sleep(3)  # Используем асинхронный sleep
-    print("Не удалось найти таблицу после нескольких попыток.")
-    return False
-
-
-# Функция для обновления цены товара в базе данных
-async def update_product_price_in_db(product_to_update, new_price):
-    async with async_session() as session:
-        async with session.begin():
-            product_to_update.current_price = new_price
-            await session.commit()
-            print(f"Цена товара обновлена в базе на {new_price} ₼")
-
-
 # Основная функция для обработки каждого товара
 async def process_product(driver, product):
     try:
-        # Извлекаем данные из product (кортеж, а не объект Product)
         product_url = product[1]  # Позиция 1 — это product_url
-        print(f"Обрабатываем товар: {product_url}")
-
+        logging.info(f"Обрабатываем товар: {product_url}")
+        
         driver.get(product_url)
-        await asyncio.sleep(2)  # Ждем загрузки страницы
+        sleep(2)  # Ждем загрузки страницы
 
         # Закрытие рекламы
         await close_ad(driver)
 
-        # Кликаем по ссылке "Посмотреть цены всех продавцов" на разных языках
+        # Кликаем по ссылке "Посмотреть цены всех продавцов" или "Bütün satıcıların qiymətlərinə baxmaq"
         click_element_by_text("Bütün satıcıların qiymətlərinə baxmaq", "Посмотреть цены всех продавцов")
 
         # Ожидаем загрузки блока с товарами
@@ -185,67 +151,89 @@ async def process_product(driver, product):
                 continue
 
         # Выводим результаты
-        print(f"Самая низкая цена: {lowest_price} от магазина {lowest_price_merchant}")
+        logging.info(f"Самая низкая цена: {lowest_price} от магазина {lowest_price_merchant}")
         if super_store_price is not None:
-            print(f"Цена от Super Store: {super_store_price}")
+            logging.info(f"Цена от Super Store: {super_store_price}")
 
-        # Обновление базы данных и изменение цены, если нужно
+        # Записываем текущую цену и последнюю проверенную цену
         async with async_session() as session:
             async with session.begin():
-                result = await session.execute(select(Product).filter(Product.product_url == product_url))
-                product_to_update = result.scalars().first()
+                product_to_update = await session.execute(select(Product).filter(Product.product_url == product_url))
+                product = product_to_update.scalars().first()
 
-                if product_to_update:
-                    product_to_update.last_checked_price = lowest_price  # Обновляем самую низкую цену конкурента
+                if product:
+                    product.current_price = super_store_price  # Записываем цену от Super Store
+                    product.last_checked_price = lowest_price  # Записываем самую низкую цену конкурента
 
-                    # Сравниваем с ценой Super Store и обновляем, если нужно
+                    # Если цена конкурента ниже, открываем ссылку для изменения цены
                     if lowest_price < super_store_price:
-                        print("Цена конкурента ниже, открываем ссылку для изменения цены.")
-                        driver.get(product_to_update.edit_url)  # Открываем ссылку для изменения
-                        await asyncio.sleep(5)
+                        logging.info("Цена конкурента ниже, открываем ссылку для изменения цены.")
+                        driver.get(product.edit_url)  # Переход по ссылке для изменения цены
+                        sleep(5)
 
-                        # Обновляем цену
-                        new_price = round(lowest_price - 0.01, 2)
-                        discount_price_input = WebDriverWait(driver, 20).until(
-                            EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Скидочная цена']"))
+                        # Ожидаем, что появится элемент с чекбоксом "Скидка"
+                        endirim_checkbox = WebDriverWait(driver, 20).until(
+                            EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Скидка')]//preceding-sibling::div"))
                         )
+
+                        try:
+                            checkmark = endirim_checkbox.find_element(By.XPATH, ".//img[@src='/checkMark.svg?url']")
+                            is_checked = checkmark.is_displayed()
+                        except:
+                            is_checked = False
+
+                        if not is_checked:
+                            logging.info("Галочка 'Скидка' не активна, активируем.")
+                            endirim_checkbox.click()
+
+                        discount_price_input = WebDriverWait(driver, 20).until(
+                            EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Скидочная цена' or @placeholder='Endirimli qiymət']"))
+                        )
+
+                        # Вводим новую цену, уменьшенную на 1 копейку
+                        new_price = round(lowest_price - 0.01, 2)
                         discount_price_input.clear()
                         discount_price_input.send_keys(str(new_price))
-                        print(f"Новая цена товара: {new_price} ₼")
 
-                        # Сохраняем изменения
+                        logging.info(f"Цена товара обновлена на {new_price} ₼")
+                        sleep(3)
+                        # Ожидаем, что кнопка "Готово" станет доступной
                         save_button = WebDriverWait(driver, 30).until(
-                            EC.element_to_be_clickable((By.XPATH, "//button[span[text()='Готово']]"))
+                            EC.element_to_be_clickable(
+                                (By.XPATH, "//button[span[text()='Готово'] or span[text()='Hazır']]")
+                            )
                         )
+                        sleep(2)
                         save_button.click()
-                        print("Цена успешно обновлена на сайте.")
+                        logging.info("Цена успешно обновлена и сохранена.")
 
-                        # Обновляем цену в базе данных
-                        await update_product_price_in_db(product_to_update, new_price)
+                        # Добавлена задержка после нажатия "Готово"
+                        sleep(10)
 
     except Exception as e:
         logging.error(f"Ошибка при обработке товара: {product[1]}: {e}")
-        driver.quit()
 
 
 # Основная функция для посещения товаров
 async def visit_products(driver):
-    if await wait_for_table_creation():  # Проверяем, существует ли таблица
-        products = await get_product_urls()  # Получаем все товары из базы данных
+    products = await get_product_urls()  # Получаем все товары из базы данных
 
-        for product in products:
-            await process_product(driver, product)
-    else:
-        print("Ошибка: таблица не существует. Завершаем работу.")
+    for product in products:
+        await process_product(driver, product)
 
 
 if __name__ == "__main__":
     try:
         # Входим в Umico Business
         login_to_umico(driver)
-        # Запускаем процесс проверки и обновления цен
-        asyncio.run(visit_products(driver))
+
+        # Создаём и запускаем цикл событий
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(visit_products(driver))
+
+    except Exception as e:
+        logging.error(f"Ошибка в основном потоке: {e}")
 
     finally:
-        # Закрываем драйвер после завершения всех операций
         driver.quit()
+        logging.info("Ты красавчик")
