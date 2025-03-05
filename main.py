bdff7be97d32c784414b1fa9d5ddae7c72a492d7
Your Name <your.email@example.com>
2025-03-06 00:47:52 +0400

    sadasdasd

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ from concurrent.futures import ThreadPoolExecutor
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # Путь к chromedriver
-chromedriver_path = r"C:/Users/Famka/vscode/selenium/chromedriver/chromedriver.exe"
+chromedriver_path = r"C:/Users/Famka/vscode/selenium/chromedriver/chromedriver"
 
 # Путь к Chrome (если установлен в стандартном месте, или укажите свой путь)
-chrome_path = r"C:/Program Files/Google/Chrome/Application/chrome.exe"  # Убедитесь, что путь правильный
+chrome_path = r"C:/Program Files/Google/Chrome/Application/chrome"  # Убедитесь, что путь правильный
 
 
 # Опции для браузера Chrome
