f5c04f8b5532ac80e73c339c2a47eb01410ab144
Your Name <your.email@example.com>
2025-03-05 10:46:59 +0400

    xvfvfdv

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ def process_products_from_json(json_file):
 if __name__ == "__main__":
     try:
         login_to_umico(driver)
-        process_products_from_json("stltnium/products.json")
+        process_products_from_json("selenium/products.json")
     except Exception as e:
         logging.error(f"Ошибка: {e}")
     finally:
