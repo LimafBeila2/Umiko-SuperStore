9d4911c62b286b425bb2a573add3c79f1acf3a7c
Your Name <your.email@example.com>
2025-03-06 05:56:08 +0400

    ываваа

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def login_to_umico(driver):
         driver.quit()
         raise ValueError("Ошибка входа! Проверь логин и пароль.")
 
+
 # Функция закрытия рекламы
 def close_ad(driver):
     try:
