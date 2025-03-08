08fec220fd9399e88a624f6a784cfccd41bdfb03
LimafBeila2 <golbolidary002@gmail.com>
2025-03-09 03:12:26 +0400

    XZZXZX

--- main.py
+++ main.py
@@ -26,11 +26,6 @@ proxies_list = [
 
 # Заголовки запроса
 headers = {
-    "Referer": "https://www.google.com/",
-    "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-    "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
 }
 
@@ -40,13 +35,13 @@ def create_driver():
     chromedriver_autoinstaller.install()
 
     options = Options()
-    options.add_argument("--headless")  # Без графического интерфейса
+    # options.add_argument("--headless")  # Без графического интерфейса
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1920x1080")
 
     # Выбираем случайный прокси
     proxy = random.choice(proxies_list)
-    options.add_argument(f"--proxy-server={proxy}")
+    # options.add_argument(f"--proxy-server={proxy}")
 
     # Создаем драйвер
     driver = webdriver.Chrome(options=options)
@@ -56,34 +51,41 @@ def create_driver():
 
     return driver  # Возвращаем драйвер с прокси и заголовками
 
+
+
 # Функция для авторизации
+# Функция для авторизации в Umico
 def login_to_umico(driver):
-    load_dotenv()
+    load_dotenv()  # Загружаем переменные окружения из файла .env
     username = os.getenv("UMICO_USERNAME")
     password = os.getenv("UMICO_PASSWORD")
 
     if not username or not password:
         raise ValueError("Ошибка: логин или пароль не найдены в .env")
 
+    # Открываем страницу авторизации
     driver.get("https://business.umico.az/sign-in")
+    
+    # Ожидаем, пока появится поле для ввода логина
     login_input = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.XPATH, "//input[@placeholder='İstifadəçi adı daxil edin']"))
     )
-    login_input.send_keys(username)
+    login_input.send_keys(username)  # Вводим логин
     
+    # Находим поле для ввода пароля и отправляем его
     password_input = driver.find_element(By.XPATH, "//input[@placeholder='Şifrəni daxil edin']")
-    password_input.send_keys(password)
-    password_input.send_keys(Keys.RETURN)
-    
+    password_input.send_keys(password)  # Вводим пароль
+    password_input.send_keys(Keys.RETURN)  # Нажимаем Enter
+
     try:
+        # Ожидаем загрузки страницы заказов, чтобы убедиться, что вход был успешным
         WebDriverWait(driver, 30).until(EC.url_contains("/account/orders"))
-        sleep(3)
+        sleep(3)  # Пауза для полной загрузки страницы
         logging.info("Успешный вход в Umico Business!")
     except:
         logging.error("Ошибка входа!")
-        driver.quit()
+        driver.quit()  # Закрываем драйвер
         raise ValueError("Ошибка входа! Проверь логин и пароль.")
-
 # Функция закрытия рекламы / выбора города
 def close_ad(driver):
     try:
@@ -99,6 +101,7 @@ def close_ad(driver):
 # Функция обработки одного товара
 def process_product(product, driver):
     try:
+        sleep(10)
         product_url, edit_url = product["product_url"], product["edit_url"]
         logging.info(f"Обрабатываем товар: {product_url}")
         driver.get(product_url)
@@ -202,10 +205,10 @@ def process_product(product, driver):
                 save_button = WebDriverWait(driver, 30).until(
                     EC.element_to_be_clickable((By.XPATH, "//button[span[text()='Готово'] or span[text()='Hazır']]"))
                 )
-
+                sleep(1)
                 save_button.click()
                 logging.info("Цена обновлена!")
-
+                sleep(1)
                 # После изменения цены пересоздаем драйвер
                 driver.quit()
                 driver = create_driver()
