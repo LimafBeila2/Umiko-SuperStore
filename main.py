81d33083fdfbd425ce3aa31591787a822ba64c59
LimafBeila2 <golbolidary002@gmail.com>
2025-03-10 23:55:29 +0400

    zxZXxzzxzxzXzX

--- main.py
+++ main.py
@@ -14,18 +14,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 import random
 import chromedriver_autoinstaller
 import pickle
+import undetected_chromedriver
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-# Список прокси
-proxies_list = [
-    "103.119.111.1:8080",
-    "103.119.111.2:8080",
-    "103.119.111.3:8080",
-]
 
-# Заголовки запроса
+
 # Заголовки запроса
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
@@ -35,6 +30,9 @@ headers = {
 CHROME_PROFILE_PATH = "/app/tmp/chrome_profile"
 
 COOKIES_PATH = "/app/tmp/cookies/cookies.pkl"
+
+
+
 
 
 def check_and_create_directory(path):
@@ -77,42 +75,37 @@ def save_cookies(driver, file_path=COOKIES_PATH):
 
 
 
-# Заголовки запроса
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
-}
-
 # Папка для хранения профиля в контейнере Railway
 CHROME_PROFILE_PATH = "/tmp/chrome_profile"
 
 
+
+
 def create_driver():
     logging.info("Создаем новый WebDriver...")
+    driver=undetected_chromedriver.Chrome()
 
-    # Автоматическая установка правильной версии ChromeDriver
-    chromedriver_autoinstaller.install()
-    logging.info("ChromeDriver успешно установлен.")
-    check_directory_access(CHROME_PROFILE_PATH)
-    check_directory_access(os.path.dirname(COOKIES_PATH))
+#     # Автоматическая установка правильной версии ChromeDriver
+#     chromedriver_autoinstaller.install()
+#     logging.info("ChromeDriver успешно установлен.")
+#     check_directory_access(CHROME_PROFILE_PATH)
+#     check_directory_access(os.path.dirname(COOKIES_PATH))
 
-    options = Options()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920x1080")
-    options.add_argument(f"--user-data-dir={CHROME_PROFILE_PATH}")  # Путь к профилю
-    options.add_argument("--headless")  # Запуск без графического интерфейса (если нужно)
+#     options = Options()
+#     options.add_argument("--no-sandbox")
+#     options.add_argument("--disable-dev-shm-usage")
+#     options.add_argument("--disable-blink-features=AutonationControlled")
+#     options.add_argument("--window-size=1920x1080")
+#     options.add_argument(f"--user-data-dir={CHROME_PROFILE_PATH}")  # Путь к профилю
+#     options.add_argument("--headless")  # Запуск без графического интерфейса (если нужно)
+#     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+#     # Выбираем случайный прокси (если используется)
+#     # proxy = random.choice(proxies_list)
+#     # options.add_argument(f"--proxy-server={proxy}")
 
-    # Выбираем случайный прокси (если используется)
-    # proxy = random.choice(proxies_list)
-    # options.add_argument(f"--proxy-server={proxy}")
-
-    # Создаем драйвер
-    driver = webdriver.Chrome(options=options)
+#     # Создаем драйвер
+#     driver = webdriver.Chrome(options=options)
     logging.info("WebDriver создан.")
-
-    # Добавляем заголовки через CDP
-    driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": headers})
-
     return driver  # Возвращаем драйвер с профилем и заголовками
 
 
