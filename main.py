b919e5b5014f822965bbc8062bc3f60c1567a021
LimafBeila2 <golbolidary002@gmail.com>
2025-03-11 01:50:55 +0400

    орполпооплпо

--- main.py
+++ main.py
@@ -253,7 +253,7 @@ def process_products_from_json(json_file):
     finally:
         driver.quit()  # Закрываем драйвер после обработки всех товаров
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     while True:
         logging.info("Запускаем процесс обработки товаров...")
         process_products_from_json("product.json")
