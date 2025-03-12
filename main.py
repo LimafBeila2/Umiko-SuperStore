48bd1fa8dfac5e4a55c1444014c377a120361c0b
LimafBeila2 <golbolidary002@gmail.com>
2025-03-12 04:54:54 +0400

    zXZXXZ

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def create_driver():
     options.add_argument(f"--user-data-dir={CHROME_PROFILE_PATH}")  # Путь к профилю
     options.add_argument("--headless")  # Без интерфейса (если нужно)
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitchers, [enable-automation]")
+    options.add_experimental_option("excludeSwitchers", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
 
     driver = webdriver.Chrome(options=options)
