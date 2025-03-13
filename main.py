7077439f0549f7d41ed492ee336ef9d6996a2fc8
LimafBeila2 <golbolidary002@gmail.com>
2025-03-14 02:07:26 +0400

    xz

--- main.py
+++ main.py
@@ -268,6 +268,7 @@ def process_product(product, driver):
             save_button = WebDriverWait(driver, 30).until(
                 EC.element_to_be_clickable((By.XPATH, "//button[span[text()='Готово'] or span[text()='Hazır']]"))
             )
+            sleep(5)
             save_button.click()
             logging.info("Кнопка 'Готово' была нажата.")
             sleep(2)
